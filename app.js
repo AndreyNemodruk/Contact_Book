321e74faf330b10da03d4c0eabfb0eeedaec1302
Андрей Немодрук <you@example.com>
2021-03-17 22:01:28 +0200

    Change server

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use("/api", require("./routes/contact.routes"));
 app.use("/api", require("./routes/Insta.routes"));
 if (process.env.NODE_ENV === "production") {
   app.use("/", express.static(__dirname + "/client/dist"));
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
